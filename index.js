8880b5f4c72d1c84b2f8c02a0e79c8e4cc190372
Iago Luciano <iago.luciano@estudante.iftm.edu.br>
2025-12-07 15:13:27 -0300

    Landing Page

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./src/configs/swaggerConfig'); 
-
 const usuarioRoutes = require('./src/routes/usuarios');
 
 app.use(express.json());
 
-// Rota para a documentação do Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // [cite: 56]
+//Configura a pasta 'public' para servir arquivos estáticos (CSS, Imagens, HTML)
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Usa as rotas importadas
+// Rota para a documentação do Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Middleware de Log
+app.use((req, res, next) => {
+  console.log("Header Content-Type:", req.headers['content-type']);
+  console.log("Body recebido:", req.body);
+  next();
+});
+
+// Usa as rotas de API
 app.use(usuarioRoutes);
+
+//Rota Raiz explícita (opcional, pois o express.static já busca o index.html, mas é bom garantir)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Acesse: http://localhost:${PORT}`);
 });
